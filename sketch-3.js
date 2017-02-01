1d38dc793bdd009d13f6d001e81e3965ed97166c
gracejisk <park.grace@cca.edu>
2017-02-01 01:46:34 -0800

    Create  sketch-3.js

---
+++ sketch-3.js
@@ -0,0 +1,14 @@
+function setup() { 
+  createCanvas(400, 400);
+} 
+
+function draw() { 
+  background(220);
+}
+var x=0;
+var y=250;
+function draw (){
+	background(300);
+	ellipse (x,y,50,50);
+	x=x+6
+}
